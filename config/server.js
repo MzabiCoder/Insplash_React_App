a8ac29f352f0bc93746a4a60fdbb78b02f45af76
Nabil Fannane <nabil@goadroit.com>
2019-04-03 12:05:02 -0700

    first commit

---
+++ config/server.js
@@ -0,0 +1,5 @@
+module.exports={
+    APPLICATION_ID: process.env.APPLICATION_ID || '871508423b22d70998d23f29f2d40445247ec5f00105b1d88cebe5d7ebffb11c',
+    secret:process.env.secret || '324f6879e7137eb1535b63434785cdcd0cd514d52b26a0e774c89bdd9fcc9dab',
+    CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+}
---
+++ server.js
@@ -0,0 +1,30 @@
+global.fetch=require('node-fetch')
+const Unsplash = require('unsplash-js').default;
+const toJson=require('unsplash-js').toJson
+const config=require('universal-config')
+
+
+
+
+const unsplash= new Unsplash({
+    applicationId:config.get('APPLICATION_ID'),
+    secret:config.get('SECRET'),
+    callbackUrl:config.get('CALLBACK_URL')
+})
+
+
+const express=require('express')
+const app=express()
+
+app.get('/api/photos',(req,res)=>{
+    unsplash.photos.listPhotos(1,30)
+    .then(toJson)
+    .then(json=>res.json(json))
+})
+
+
+const PORT=process.env.PORT || 5000
+
+app.listen(PORT,()=>{
+    console.log(`Server up and running on port ${PORT}`)
+})
